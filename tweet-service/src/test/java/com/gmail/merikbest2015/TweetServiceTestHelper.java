943b37d3e96de2e4483182caf625e5716c820f4f
Sharmistha Karmakar <sk.20u10166@btech.nitdgp.ac.in>
2023-08-19 11:59:21 +0530

    Finally added project

---
+++ tweet-service/src/test/java/com/gmail/merikbest2015/TweetServiceTestHelper.java
@@ -0,0 +1,76 @@
+package com.gmail.merikbest2015;
+
+import com.gmail.merikbest2015.dto.request.NotificationRequest;
+import com.gmail.merikbest2015.dto.response.tweet.TweetAuthorResponse;
+import com.gmail.merikbest2015.dto.response.tweet.TweetListResponse;
+import com.gmail.merikbest2015.enums.LinkCoverSize;
+import com.gmail.merikbest2015.enums.NotificationType;
+import com.gmail.merikbest2015.enums.ReplyType;
+import com.gmail.merikbest2015.model.GifImage;
+import com.gmail.merikbest2015.model.Poll;
+import com.gmail.merikbest2015.model.Tweet;
+import com.gmail.merikbest2015.repository.projection.TweetUserProjection;
+import com.gmail.merikbest2015.util.TestConstants;
+import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TweetServiceTestHelper {
+
+    private static final ProjectionFactory factory = new SpelAwareProxyProjectionFactory();
+
+    public static <T> T createTweetProjection(Class<T> type) {
+        Map<String, Object> tweetMap = new HashMap<>();
+        tweetMap.put("id", TestConstants.TWEET_ID);
+        tweetMap.put("text", TestConstants.TWEET_TEXT);
+        tweetMap.put("dateTime", LocalDateTime.now());
+        tweetMap.put("scheduledDate", LocalDateTime.now());
+        tweetMap.put("addressedUsername", TestConstants.USERNAME);
+        tweetMap.put("addressedId", TestConstants.USER_ID);
+        tweetMap.put("addressedTweetId", 123L);
+        tweetMap.put("replyType", ReplyType.EVERYONE);
+        tweetMap.put("link", TestConstants.LINK);
+        tweetMap.put("linkTitle", TestConstants.LINK_TITLE);
+        tweetMap.put("linkDescription", TestConstants.LINK_DESCRIPTION);
+        tweetMap.put("linkCover", TestConstants.LINK_COVER);
+        tweetMap.put("gifImage", new GifImage());
+        tweetMap.put("linkCoverSize", LinkCoverSize.LARGE);
+        tweetMap.put("authorId", TestConstants.USER_ID);
+        tweetMap.put("listId", TestConstants.LIST_ID);
+        tweetMap.put("images", new ArrayList<>());
+        tweetMap.put("imageDescription", "");
+        tweetMap.put("quoteTweet", new Tweet());
+        tweetMap.put("poll", new Poll());
+        tweetMap.put("deleted", false);
+        tweetMap.put("user", new TweetAuthorResponse());
+        tweetMap.put("tweetList", new TweetListResponse());
+        tweetMap.put("taggedImageUsers", new ArrayList<>());
+        tweetMap.put("isTweetLiked", false);
+        tweetMap.put("isTweetRetweeted", false);
+        tweetMap.put("isTweetBookmarked", false);
+        tweetMap.put("isUserFollowByOtherUser", false);
+        tweetMap.put("retweetsCount", 1);
+        tweetMap.put("likedTweetsCount", 1);
+        tweetMap.put("repliesCount", 0);
+        tweetMap.put("quotesCount", 0);
+        if (type.equals(TweetUserProjection.class)) {
+            tweetMap.put("retweetsUserIds", List.of(1L, 2L));
+        }
+        return factory.createProjection(type, tweetMap);
+    }
+
+    public static NotificationRequest createMockNotificationRequest(NotificationType notificationType, boolean notificationCondition) {
+        return NotificationRequest.builder()
+                .notificationType(notificationType)
+                .notificationCondition(notificationCondition)
+                .notifiedUserId(TestConstants.USER_ID)
+                .userId(TestConstants.USER_ID)
+                .tweetId(TestConstants.TWEET_ID)
+                .build();
+    }
+}
